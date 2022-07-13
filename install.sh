90b31a32b877f432377759215f2d838c88e65c29
Lukes <luke_keam@hotmail.com>
2022-07-13 14:49:57 +1000

    .901

--- install.sh
+++ install.sh
@@ -2,11 +2,14 @@
 # install git sed xdotool
 sudo apt-get update
 sudo apt-get install git sed xdotool -y
+# make dir
+sudo mkdir /pi-kiosk
+cd /pi-kiosk
 # clone repository
 echo "downloading pi-kiosk"
-git clone https://github.com/LukeKeam/pi-kiosk
+git clone https://github.com/LukeKeam/pi-kiosk .
 # change ownership
-sudo chown -R "$USER":"$USER" pi-kiosk
+sudo chown -R "$USER":"$USER" /pi-kiosk
 # make service so it starts automatically
 echo "making service"
 append_line='[Unit]
@@ -18,7 +21,7 @@ After=graphical.target
 Environment=DISPLAY=:0.0
 Environment=XAUTHORITY=/home/pi/.Xauthority
 Type=simple
-ExecStart=/bin/bash ~/pi-kiosk/pi-kiosk.sh
+ExecStart=/bin/bash /pi-kiosk/pi-kiosk.sh
 Restart=on-abort
 
 [Install]
