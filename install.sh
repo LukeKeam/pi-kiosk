bb2223916d26e581faf9742c4cf2362beba4e564
Lukes <luke_keam@hotmail.com>
2022-07-13 14:45:21 +1000

    .9

--- install.sh
+++ install.sh
@@ -1,6 +1,4 @@
-# easy install options curl -LJO https://raw.githubusercontent.com/LukeKeam/pi-kiosk/master/install.sh && sudo sh ./install.sh
-# create dir /pi-enviro
-echo "make directory /pi-kiosk "
+# easy install options curl -LJO https://raw.githubusercontent.com/LukeKeam/pi-kiosk/master/install.sh && sudo bash ./install.sh
 # install git sed xdotool
 sudo apt-get update
 sudo apt-get install git sed xdotool -y
